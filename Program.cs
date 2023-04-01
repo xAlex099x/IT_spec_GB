94de3ea70cdc586e524b74511b729856c811966d
xAlex099x <Alex09920000@gmail.com>
2023-04-01 19:13:33 +0900

    добавлена диаграмма, доработано решение

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿int lengthWord = 3;
+
+string[] startArray = { "Hello", "See", "Work", "Smile", "Ufc" , ":=)"};
+string[] tempArray = new string[startArray.Length];
+int count = 0;
+
+//Добавляем во временный массив значения, нужного размера.
+//Считаем кол-во слов.
+
+for (int i = 0, y = 0; i < startArray.Length; i++)
+{
+    if (startArray[i].Length <= lengthWord)
+    {
+        tempArray[y] = startArray[i];
+        y++;
+        count++;
+    }
+}
+
+string[] finalArray = new string[count + 1];
+
+//В финальный массив добавляем значения
+for (int i = 0; i <= count; i++)
+{
+    finalArray[i] = tempArray[i];
+}
+
+//Выводим финальный массив
+for (int i = 0; i < finalArray.Length - 1; i++)
+{
+    Console.WriteLine($"{i} - > {finalArray[i]};");
+}
